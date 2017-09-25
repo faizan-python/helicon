c2d7b58a23be21a3caddc1473b64e3fc94efa74a
faizans-cuelogic <faizan.shaikh@cuelogic.co.in>
2017-09-25 17:04:46 +0530

    Updated tax details. Updated tax invoice details.

--- service/admin.py
+++ service/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from service.models import Service
+from service.models import Service, InvoiceDetail
 
 admin.site.register(Service)
+admin.site.register(InvoiceDetail)
